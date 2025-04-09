39ba82819dcafc977164f8a6ad1cf275a364632d
louisboucard <louis44.boucard@gmail.com>
2025-04-09 08:10:15 +0200

    trt

--- src/main/java/fr/epsi/b3devc1/msprapi/repository/ContinentRepository.java
+++ src/main/java/fr/epsi/b3devc1/msprapi/repository/ContinentRepository.java
@@ -6,6 +6,5 @@ import java.util.Optional;
 
 public interface ContinentRepository extends JpaRepository<Continent, Integer> {
 
-    // Recherche un continent par son nom
     Optional<Continent> findByName(String name);
 }
